33f069cc7ca926ce3e842cd8557a2f37a4e55266
Stepan <38866661+STEPANONEDEATH@users.noreply.github.com>
2025-01-21 21:20:22 +0500

    Update urls.py

--- UI_UX_Project/ui_ux_project/urls.py
+++ UI_UX_Project/ui_ux_project/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
 
     # Админ-панель и аутентификация
     path('admin/', admin.site.urls),                                    # Панель администрирования
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),     # Страница входа
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),                            # Страница выхода
 ]
 
 # Добавление маршрутов для работы с загружаемыми файлами
